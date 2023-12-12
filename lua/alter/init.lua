e896d46f6cbc2b39c90ac33bbae20a0a780ebe54
Sawan Sunar <Sawansunar24072002@gmail.com>
2023-12-13 04:05:12 +0530

    Buffer loaded and buf listed

--- lua/alter/init.lua
+++ lua/alter/init.lua
@@ -4,6 +4,21 @@ local data_path = vim.fn.stdpath("data")
 local user_config = string.format("%s/alter.json", config_path)
 local cache_config = string.format("%s/alter.json", data_path)
 
+-- util function sections
+-- prints out all given table
+local function printAllElements(tbl)
+    for key, value in pairs(tbl) do
+        if type(value) == "table" then
+            print("Table found at key:", key)
+            printAllElements(value)  -- Recursively call function for nested tables
+        else
+            print(key, value)  -- Print other types of values
+        end
+    end
+end
+
+-- Class declarations sections
+
 ---@class AlterData
 ---@field tbl {}
 local alterData = {
@@ -16,12 +31,20 @@ local AlterConfig = {
     data = alterData
 }
 
+-- class methods declarations
+
 function alterData:print()
     for i, v in ipairs(self.tbl) do
         print(i.. ", " .. v .. "\n")
     end
 end
 
+
+-- reads cache_config and puts it to alterData.tbl and prints out the values.
+function alterData:Load()
+    self.tbl = vim.json.decode(Path:new(cache_config):read())
+    printAllElements(self.tbl)
+end
 local function project_key()
     return vim.loop.cwd()
 end
@@ -34,7 +57,7 @@ local function get_data(bufnr)
     return Path:new(bufnr):make_relative(project_key())
 end
 
-local function save_to_cache() 
+local function save_to_cache()
     Path:new(cache_config):write(vim.fn.json_encode(AlterConfig.data.tbl), "w")
 end
 
@@ -43,10 +66,22 @@ local function set_current_buffer()
     table.insert(AlterConfig.data.tbl, slot)
 end
 
-local function set_bufnr()
-    local bufnr = vim.fn.bufnr(AlterConfig.data.tbl[1])
-    if bufnr ~= nil then
-        vim.api.nvim_set_current_buf(bufnr)
+---@param num integer
+local function set_bufnr(num)
+    local bufnr = vim.fn.bufnr(AlterConfig.data.tbl[num])
+    if bufnr == -1 then
+         bufnr = vim.fn.bufnr(AlterConfig.data.tbl[num], true)
+    end
+
+    -- have to make it work
+    -- vim.api.nvim_command('buffer ' .. bufnr)
+    -- vim.api.nvim_buf_is_loaded(bufnr)
+
+    if not vim.api.nvim_set_current_buf(bufnr) then
+        vim.fn.bufload(bufnr)
+        vim.api.nvim_set_option_value("buflisted", true, {
+            buf = bufnr,
+        })
     end
 end
 
@@ -54,21 +89,7 @@ local function print_buffer()
     AlterConfig.data:print()
 end
 
-local function printAllElements(tbl)
-    for key, value in pairs(tbl) do
-        if type(value) == "table" then
-            print("Table found at key:", key)
-            printAllElements(value)  -- Recursively call function for nested tables
-        else
-            print(key, value)  -- Print other types of values
-        end
-    end
-end
 
-local function AlterDataLoad()
-    alterData.tbl = vim.json.decode(Path:new(cache_config):read())
-    printAllElements(alterData.tbl)
-end
 
 return {
     data = get_data,
@@ -76,5 +97,6 @@ return {
     print = print_buffer,
     set = set_bufnr,
     cac = save_to_cache,
-    pro = AlterDataLoad,
+    alterData = alterData,
+    alterConfig = AlterConfig,
 }
